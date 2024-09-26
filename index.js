57b9cf4dc17b5cca59da41aaa77657bc8c8ee410
Monica Fernandez Salinas <monica.fernandez.salinas@gmail.com>
2024-09-26 21:23:31 +0200

    Lab Solution

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "Leopoldo"
+console.log("The driver's name is" $(hacker1));
+
+const hacker2 = "Raimundo"
+console.log("The navigator's name is" $(hacker2));
 
 // Iteration 2: Conditionals
+
+
+
 
 
 // Iteration 3: Loops
